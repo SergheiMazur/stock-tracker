bec196d27bcae8b102e4f293d4ffd20f3e91b033
Serghei Mazur <serhio1007@gmail.com>
2018-06-17 06:22:22 +0000

    Add authentication for API

---
+++ app/controllers/api/v1/sessions_controller.rb
@@ -0,0 +1,25 @@
+module Api
+  module V1
+    
+    class SessionsController < ApplicationController
+      skip_before_action :verify_authenticity_token, only: [:create]
+      
+      def create
+        user = User.where(email: params[:email]).first
+        
+        if user.valid_password?(params[:password])
+          render json: user.as_json(only: [:id, :email]),status: :created
+        else
+          head(:unauthorized)
+        end
+        
+      end
+      
+      def destroy
+        
+      end
+      
+    end
+    
+  end
+end
