6fc98bf3f0a97f840260adc7342caf6bec12a5d7
Serghei Mazur <serhio1007@gmail.com>
2018-06-20 04:28:41 +0000

    Add jbuilder for session json

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -6,10 +6,10 @@ module Api
       skip_before_action :authenticate_user!, only: [:create]
       
       def create
-        user = User.where(email: params[:email]).first
+        @user = User.where(email: params[:email]).first
         
-        if user && user.valid_password?(params[:password])
-          render json: user.as_json(only: [:email, :authentication_token]),status: :created
+        if @user && @user.valid_password?(params[:password])
+          render :create, status: :created
         else
           head(:unauthorized)
         end
